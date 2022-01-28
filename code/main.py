9a61fc044bced7244216779911583ad0b74a385d
Yi Qin <qinyi_linxuan@126.com>
2022-01-27 18:11:08 -0800

    make it work on multiple versions

--- code/main.py
+++ code/main.py
@@ -2,6 +2,9 @@ import cal_CloudRadKernel as CRK
 import organize_jsons as OJ
 import cld_fbks_ecs_assessment_v3 as dataviz
 import os
+import cases_lookup as CL
+import pickle
+import json
 
 # User Input:
 #================================================================================================
@@ -12,70 +15,89 @@ grid_label = 'gr1'   ### not necessary to be changed generally.
 
 # you can set version as a tag for different sensitivity experiments. The following casename for 
 # control and warming will correspond to your version name. @@@@@ change here..
-version = 'F2010v2rc1c'
-#version = 'F2010v1'
+versions = ['F2010v2rc1c']
 
-# directionary of your input model data 
-# [you need to run main.py in diag_feedback_e3sm package first with PreProces = True to get all needed data here.]
-path = '/p/lustre2/qin4/diag_feedback_E3SM_postdata'
+newmodels = []
+for iversion,version in enumerate(versions):
 
-# set simulation length: (start, end)
-tslice = ("0001-01-01", "0005-12-31")
+    newmodels.append(model+'_'+version)
 
-fields = ['tas','rsdscs','rsuscs','OMEGA','FISCCP1_COSP'] # don't change this. 
+    #continue
+
+    # directionary of your input model data 
+    # [you need to run main.py in diag_feedback_e3sm package first with PreProces = True to get all needed data here.]
+    path = '/p/lustre2/qin4/diag_feedback_E3SM_postdata'
+    
+    # set simulation length: (start, end)
+    tslice = ("0001-01-01", "0005-12-31")
+    
+    fields = ['tas','rsdscs','rsuscs','OMEGA','FISCCP1_COSP'] # don't change this. 
+    
+    
+    # generate xmls pointing to the cmorized netcdf files 
+    os.system('mkdir ../xmls/')
+    
+    filenames={}
+    for exp in ['amip','amip-p4K']:
+        filenames[exp]={}
+        if exp=='amip':
+            activity = 'CMIP'
+            #######  set the control case name @@@@@ change here..
+            casename = CL.get_lutable(version,exp)
+        else:
+            activity = 'CFMIP'
+            #######  set the warming case name @@@@@ change here..
+            casename = CL.get_lutable(version,exp)
+   
+        print('casename=',casename)
+    
+    #================================================================================================
+        for field in fields:
+            #if field=='clisccp':
+            if field=='FISCCP1_COSP':
+                table='CFmon'
+            else:
+                table='Amon'
+    
+            searchstring = path+'/'+casename+'/'+field+'_*.nc'
+            xmlname = '../xmls/'+exp+'.'+model+'.'+field+'.'+version+'.xml'
+    
+            if os.path.isfile(xmlname):
+                print('========= ',xmlname,' is available now =======================')
+            else:
+                os.system('cdscan -x '+xmlname+' '+searchstring)
+    
+            filenames[exp][field] = xmlname
 
 
-# generate xmls pointing to the cmorized netcdf files 
-os.system('mkdir ../xmls/')
+    # calculate all feedback components and Klein et al (2013) error metrics:
+    fname = '../data/backup_dict_'+version+'.pickle'
+    if not os.path.isfile(fname):
+        fbk_dict,obsc_fbk_dict,err_dict = CRK.CloudRadKernel(filenames,fields,tslice) 
 
-filenames={}
-for exp in ['amip','amip-p4K']:
-    filenames[exp]={}
-    if exp=='amip':
-        activity = 'CMIP'
-        #######  set the control case name @@@@@ change here..
-        if version == 'F2010v2rc1c':
-            casename = '20210423.v2rc1c.F2010.ne30pg2_oECv3.chrysalis' 
-        elif version == 'F2010v1':
-            casename = '20210101.F2010C5-CMIP6-LR.ne30_oECv3.syrah.1024'
+        # save dict into pickle as a backup
+        fout = open(fname,'wb')
+        pickle.dump([fbk_dict,obsc_fbk_dict,err_dict],fout)
+        fout.close()
+
     else:
-        activity = 'CFMIP'
-        #######  set the warming case name @@@@@ change here..
-        if version == 'F2010v2rc1c':
-            casename = '20210423.v2rc1c.F2010plus4K.ne30pg2_oECv3.chrysalis' 
-        elif version == 'F2010v1':
-            casename = '20210101.F2010C5-CMIP6-LR-p4K.ne30_oECv3.syrah.1024'
+        print('Data is ready for',version)
+    
 
-#================================================================================================
-    for field in fields:
-        #if field=='clisccp':
-        if field=='FISCCP1_COSP':
-            table='CFmon'
-        else:
-            table='Amon'
+# organize all data into json file for further making figures
+for iversion,version in enumerate(versions):
 
-        searchstring = path+'/'+casename+'/'+field+'_*.nc'
-        xmlname = '../xmls/'+exp+'.'+model+'.'+field+'.'+version+'.xml'
+    fin = open('../data/backup_dict_'+version+'.pickle','rb')
+    fbk_dict,obsc_fbk_dict,err_dict = pickle.load(fin)
+    fin.close()
 
-        if os.path.isfile(xmlname):
-            print('========= ',xmlname,' is available now =======================')
-        else:
-            os.system('cdscan -x '+xmlname+' '+searchstring)
-
-        filenames[exp][field] = xmlname
-
-
-
-# calculate all feedback components and Klein et al (2013) error metrics:
-fbk_dict,obsc_fbk_dict,err_dict = CRK.CloudRadKernel(filenames,fields,tslice) 
-
-# add this model to the pre-existing json file containing other model results: 
-updated_fbk_dict,updated_obsc_fbk_dict = OJ.organize_fbk_jsons(fbk_dict,obsc_fbk_dict,model+'_'+version,variant)
-updated_err_dict = OJ.organize_err_jsons(err_dict,model+'_'+version,variant)
+    # add this model to the pre-existing json file containing other model results: 
+    updated_fbk_dict,updated_obsc_fbk_dict = OJ.organize_fbk_jsons(fbk_dict,obsc_fbk_dict,model+'_'+version,variant,flag=iversion)
+    updated_err_dict = OJ.organize_err_jsons(err_dict,model+'_'+version,variant,flag=iversion)
 
 # plot this model alongside other models and expert assessment:
 os.system('mkdir ../figures/'+version+'/')
 figdir = '../figures/'+version+'/'
-result = dataviz.make_all_figs(updated_fbk_dict,updated_obsc_fbk_dict,updated_err_dict,model+'_'+version,figdir)
+result = dataviz.make_all_figs(updated_fbk_dict,updated_obsc_fbk_dict,updated_err_dict,newmodels,figdir,onlytest=True)
 
 print('Done!')
